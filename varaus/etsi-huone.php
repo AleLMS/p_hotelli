0f75a9e6d6e68e2de67b03c0ebc5a301a458adfd
AleLMS <s3laal00@students.osao.fi>
2024-01-16 03:45:00 +0200

    updated varaus site

--- varaus/etsi-huone.php
+++ varaus/etsi-huone.php
@@ -26,6 +26,7 @@ function get_available_rooms($hotel, $size, $startDate, $endDate)
                         ? BETWEEN alku_pvm AND loppu_pvm
                         OR
                         ? < alku_pvm and ? > loppu_pvm);');
+
     $stmt->bind_param('iissss', $hotel, $size, $startDate, $endDate, $startDate, $endDate);
     $stmt->execute();
 
@@ -44,89 +45,6 @@ function get_available_rooms($hotel, $size, $startDate, $endDate)
     return $availableRooms;
 }
 
-// DEPRECATE
-/* function get_all_rooms($hotel, $size)
-{
-    // connect to DB
-    $searchDB = connect_to_db("p_hotelli_test");
-
-    $stmt = $searchDB->prepare('SELECT * FROM huoneet WHERE hotelli_ID = ? AND vuodepaikat >= ?');
-    $stmt->bind_param('ii', $hotel, $size);
-    $stmt->execute();
-
-    $result = $stmt->get_result();
-
-    if ($result->num_rows <= 0) return null;
-
-    while ($row = $result->fetch_assoc()) {
-        $rooms[] = $row;
-    }
-
-    // disconnect from DB
-    $searchDB->close();
-
-    return $rooms;
-}
-
-function get_booked_rooms($hotel, $size, $startDate, $endDate)
-{
-    // connect to DB
-    $searchDB = connect_to_db("p_hotelli_test");
-
-    // search DB with $searchData
-    $bookedRoomsQuery = $searchDB->prepare('SELECT *
-                            FROM huoneidenvaraukset
-                            INNER JOIN huoneet
-                            ON huoneidenvaraukset.huone_ID = huoneet.huone_ID
-                            WHERE 
-                            hotelli_ID = ? AND vuodepaikat >= ? AND ? BETWEEN alku_pvm AND loppu_pvm 
-                            OR
-                            hotelli_ID = ? AND vuodepaikat >= ? AND ? BETWEEN alku_pvm AND loppu_pvm
-                            OR
-                            hotelli_ID = ? AND vuodepaikat >= ? AND ? < alku_pvm AND ? > loppu_pvm;');
-
-    $bookedRoomsQuery->bind_param('iisiisiiss', $hotel, $size, $startDate, $hotel, $size, $endDate, $hotel, $size, $startDate, $endDate);
-    $bookedRoomsQuery->execute();
-
-    $result = $bookedRoomsQuery->get_result();
-
-    if ($result->num_rows <= 0) return null;
-
-    // get available rooms to an array
-    while ($row = $result->fetch_assoc()) {
-        $bookedRooms[] = $row;
-    }
-
-    // disconnect from DB
-    $searchDB->close();
-
-    return $bookedRooms;
-}
-
-function filter_booked_rooms($a, $b)
-{
-    // optimize later
-
-    $result = (array)null;
-    $idsToRemove = (array)null;
-    foreach ($b as $booked) {
-        array_push($idsToRemove, (int)$booked['huone_ID']);
-    }
-
-    foreach ($a as $room) {
-        $filterThis = false;
-        foreach ($idsToRemove as $id) {
-            if ((int)$room['huone_ID'] === $id) {
-                $filterThis = true;
-                break;
-            }
-        }
-        if (!$filterThis) array_push($result, $room);
-    }
-
-    return $result;
-} */
-
 ?>
 
 <?php
@@ -134,6 +52,8 @@ function filter_booked_rooms($a, $b)
 
 // receive data from the front-end
 $searchData = json_decode(file_get_contents("php://input"), true);
+
+if (empty($searchData)) exit(json_encode("Empty input."));
 
 // Bind data
 $hotel = $searchData['location'];
@@ -145,6 +65,8 @@ $endDate = $searchData['endDate'];
 $availableRooms = get_available_rooms((int)$hotel, (int)$size, $startDate, $endDate);
 
 // send rooms to the front-end
-if (empty($availableRooms)) exit(json_encode("No rooms found."));
-else echo (json_encode($availableRooms));
+if (empty($availableRooms))
+    exit(json_encode("No rooms found."));
+else
+    echo (json_encode($availableRooms));
 ?>
--- varaus/varaus.js
+++ varaus/varaus.js
@@ -4,46 +4,94 @@ import { Room } from "./huone.js";
 window.addEventListener('load', function () {
     console.log("Document loaded.");
 
+    // SAMPE -> ?sijainti=1&huoneenKoko=1&startDate=1990-11-11&endDate=1990-12-12&sendSearch=Hae
+    const urlSearch = window.location.search;
+    if (urlSearch) crossSiteSearch(urlSearch);
+
     // Get post
     document.forms['searchForm'].addEventListener('submit', function (event) {
         // Interrupt default post behavior
         event.preventDefault();
 
+        let location = document.getElementById("sijainti").value;
+        let roomSize = document.getElementById("huoneenKoko").value;
+        let startDate = document.getElementById("startDate").value;
+        let endDate = document.getElementById("endDate").value;
+
         // AJAX
-        ajaxSearch();
+        ajaxSearch(location, roomSize, startDate, endDate);
+
     });
 
 }, false);
 
-function ajaxSearch() {
+
+// Functions
+function ajaxSearch(location, roomSize, startDate, endDate) {
     let ajax = new XMLHttpRequest();
 
     ajax.open('POST', 'etsi-huone.php', true);
 
     ajax.onload = async () => {
         if (ajax.status === 200) {
-            console.log(ajax.responseText);
             let results = JSON.parse(ajax.responseText);
-            console.log(results);
             document.getElementById('roomsContainer').innerHTML = '<div id="displayRoomsAfter" style="display: none; "></div>';
             await results.forEach((room) => AddNewRoom(room));
+            updateResultText();
         } else {
-            console.log(ajax.status + " " + ajax.statusText);
+            throw new Error(ajax.status + ":" + ajax.responseText + " | Bad request.");
         }
     }
 
-    //console.log(Object.fromEntries(document.forms['searchForm']));
-
+    // Send JSON
     const searchData = {
-        location: document.getElementById("sijainti").value,
-        roomSize: document.getElementById("huoneenKoko").value,
-        startDate: document.getElementById("startDate").value,
-        endDate: document.getElementById("endDate").value
+        location: location,
+        roomSize: roomSize,
+        startDate: startDate,
+        endDate: endDate
     }
 
+    console.log("asdsd");
+    console.log(searchData);
     const jsonData = JSON.stringify(searchData);
-
     ajax.send(jsonData);
+
+    // Update URL to allow saving search data (history, bookmarks)
+    let urlSearch = "?sijainti=" + location + "&huoneenKoko=" + roomSize + "&startDate=" + startDate + "&endDate=" + endDate + "&sendSearch=Hae";
+    history.replaceState(null, null, urlSearch)
+}
+
+function crossSiteSearch(urlData) {
+
+    // Parse URL data
+    urlData = urlData.replace("?", "");
+    urlData = urlData.split('&');
+
+    // Instantiate array
+    let search = [];
+
+    // Splits search data to an array
+    for (let i = 0; i < urlData.length; i++) {
+        search[i] = urlData[i].split("=");
+    }
+
+    // Assign url data
+    // Nasty assignment logic, fix later (search[entry][value])
+    let location = search[0][1];
+    let roomSize = search[1][1];
+    let startDate = search[2][1];
+    let endDate = search[3][1];
+
+    // Search
+    ajaxSearch(location, roomSize, startDate, endDate);
+
+}
+
+function updateResultText() {
+    // Update results text
+    let resultText = document.getElementById("numResults");
+    let numRooms = document.getElementById("roomsContainer").childElementCount - 1; // -1 for the hidden element
+    resultText.innerHTML = "Löytyi " + numRooms + " huonetta"
 }
 
 async function AddNewRoom(input) {
