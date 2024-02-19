edf981730dffc8f5567d4f0db1e3a8b2a12e07e1
Lispehol <146867390+Lispehol@users.noreply.github.com>
2024-02-19 08:40:40 +0200

    uusin

--- vieraskirja/vieraskirja.js
+++ vieraskirja/vieraskirja.js
@@ -3,40 +3,26 @@ function rating(p){
 
     if(paarynat == 1){
        document.getElementById("arvio").value = "1";
-        $(document).ready(function(){
-        $("#p1").click(function(){
             $("#p1, #p2, #p3, #p4, #p5").css("opacity", "1");
             $("#p2, #p3, #p4, #p5").css("opacity", "0.2");
-            });
-        });
     }
     else if(paarynat == 2){
         document.getElementById("arvio").value = "2";
-        $(document).ready(function(){
-            $("#p2").click(function(){
                 $("#p1, #p2, #p3, #p4, #p5").css("opacity", "1");
-                $("#p3, #p4, #p5").css("opacity", "0.2");
-                });
-            });
+               $("#p3, #p4, #p5").css("opacity", "0.2");
+    
         
     }
     else if(paarynat == 3){
         document.getElementById("arvio").value = "3";
-        $(document).ready(function(){
-            $("#p3").click(function(){
                 $("#p1, #p2, #p3, #p4, #p5").css("opacity", "1");
                 $("#p4, #p5").css("opacity", "0.2");
-                });
-            });
+
     }
     else if(paarynat == 4){
         document.getElementById("arvio").value = "4";
-        $(document).ready(function(){
-            $("#p4").click(function(){
                 $("#p1, #p2, #p3, #p4, #p5").css("opacity", "1");
                 $("#p5").css("opacity", "0.2");
-                });
-            });
     }
     else {
         document.getElementById("arvio").value = "5";
--- vieraskirja/vieraskirja.php
+++ vieraskirja/vieraskirja.php
@@ -8,23 +8,19 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-  
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Quicksand Medium">
   <link rel="stylesheet" href="vieraskirja.css">
-  <link rel="stylesheet" href="global.css">
   
 </head>
 <body>
 
     <?php
 
-//Create connection
- 
+    //Create connection
     require 'settings.php';
     $conn = new mysqli($servername, $username, $password, $dbname);
-    
     $result = mysqli_query($conn, "SELECT * FROM messages ORDER BY id DESC limit 5");
-           $messages = mysqli_fetch_all($result, MYSQLI_ASSOC);
-           
+           $messages = mysqli_fetch_all($result, MYSQLI_ASSOC);   
 
 
     if(isset($_POST["submit"])){
@@ -46,7 +42,6 @@
 
 
         $conn->close();
-  /*   */   
     ?>
   <nav class="navbar navbar-expand-sm navbar-dark" style="background-color: #9cda71; padding: 0.75em; font-size: 1.2em;">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -55,14 +50,18 @@
 
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="">Etusivu</a></li>
-        <li class="nav-item">
-          <a class="nav-link" href="">Galleria</a></li>
-        <li class="nav-item">
-          <a class="nav-link" href="">Vieraskirja</a></li>
-        <li class="nav-item">
-          <a class="nav-link" href="">Yhteystiedot</a></li>
+      <li class="nav-item topimg hidden-xs hidden-sm">
+                  <a class="nav-link" aria-current="page" href=""><img src="../media/paaryna.png" style="width: 60px; height: 60px;" alt="">
+                  <img src="../media/paarynalogo.png" style="width: 220px; height: 50px" alt=""></a></li>
+      <ul class="navbar-nav nav justify-content-center"></ul>
+        <li class="nav-item mt-2">
+          <a class="nav-link active" aria-current="page" href="mainpage.php">Etusivu</a></li>
+        <li class="nav-item mt-2">
+          <a class="nav-link" href="gallery.html">Galleria</a></li>
+        <li class="nav-item mt-2">
+          <a class="nav-link" href="#">Vieraskirja</a></li>
+        <li class="nav-item mt-2">
+          <a class="nav-link" href="yhteystiedot.html">Yhteystiedot</a></li>
       </ul>
     </div>
 </nav>
@@ -73,67 +72,55 @@
         <img class="image" src="vieraskirjantausta.png" class="img-fluid" alt="Image not found" width="100%" height="100%">
        --> 
         <!-- tekstikentät-->
-        <div class="container-fluid" id="mainContainer" style="background-image: url('vieraskirjantausta.png'); background-position: center;
+        <div class="container-fluid" id="mainContainer" style="background-image: url(../media/vieraskirjantausta.png); background-position: center;
         background-repeat: no-repeat; background-size: cover; position: relative;">
-            <h1 class="h1">Vieraskirja</h1><br>
-            <table class="table table-light table-borderless" id="table" style="border-radius: 5px; opacity: 0.7;">
-              <thead>
-                <th>Nimi</th>
-                <th>Palaute</th>
-                <th>Aika</th>
-                <th>Arvio</th>
-              </thead>
-              <tbody>
-              <?php foreach ($messages as $message): ?>
-                <tr>
-                  <td> -----------------</td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                </tr>
-                <tr>
-                    <td><b> <?php echo $message['nimi']; ?> </b></td>
-                    <td><i>"<?php echo $message['viesti'] ?>"</i> </td>
-                    <td><b> <?php echo $message['aika']; ?></b> </td>
-                    <td> <?php for ($i = 1; $i <= $message['arvio']; $i++){
-                    echo '<img src="paaryna.png" width="30px" height="30px">';
-                    }?></td>
-                </tr>
-                <?php endforeach; ?>
-                
-              
-              </tbody>
-              
-            </table>
-            <h4 class="text-white" >Kirjoita vieraskirjaan kokemuksestasi hotellissamme täyttämällä alla oleva lomake. Voit arvioida kokemuksesi päärynöillä.</h4>
+            <h1 class="h1 pt-5 mx-auto" style="font-size: min(8svh, 8svw); color: #ffffff; margin-bottom:15px;">Vieraskirja</h1>
+            
+    <div class="msgcontainer container mt-5 rounded-2" style=" min-height: 250px; background-color: white; border-radius: 5px; opacity: 0.7"><?php foreach ($messages as $message): ?>
+    <div class="msgarea row pt-4 pb-5" style="height:5em; ">
+              <div class="col-6 col-sm-6 col-lg-3 col-md-3">
+                <p><b><?php echo $message['aika']; ?></b></p>
+              </div>
+              <div class="col-6 col-sm-6 col-lg-3 col-md-3">
+                <p><b><?php echo $message['nimi']; ?></b></p>
+              </div>
+              <div class="col-6 col-xs-6 col-sm-6 col-lg-4 col-md-3">
+                <p><b><i>"<?php echo $message['viesti'] ?>"</i></b> </p>
+              </div>
+              <div class="col-6 col-xs-6 col-sm-6 col-lg-2 col-md-3">
+                <p><?php for ($i = 1; $i <= $message['arvio']; $i++){
+                      echo '<img src="../media/paaryna.png" width="30px" height="30px">';
+                      }?></p>
+              </div>
+            </div><?php endforeach; ?>
+    </div>
+         
+            <div class="row" style="height: 7svh;"></div> <!-- spacer -->
+            <div class="form container">
+            <h4 class="h4 text-white mb-4" style="text-shadow: 1px 1px 2px black, 0 0 25px #b8afae, 0 0 5px;">Kirjoita vieraskirjaan kokemuksestasi hotellissamme täyttämällä alla oleva lomake. Voit arvioida kokemuksesi päärynöillä.</h4>
                <!-- form   -->
                 <form id="lomake" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
       
                    <div class="row mt-3 text-center" id="input">
-                        <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mb-4"><input class="form-control form-control-lg" id="nimikentta" type="text" placeholder="Kirjoita tähän nimesi.." name="name"></div>
-                        <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mb-4"><input class="form-control form-control-lg"  id="tekstikentta" type="text" placeholder="Kirjoita tähän viesti..." name="message"></div>
-                        <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mb-4" >
-                        <button type="button" class="btn paarynat " onclick="rating(1);" id="paaryna1"><img  src="paaryna.png" width="30px" height="30px" alt="Image not found" id="p1"></button>
-                        <button type="button" class="btn paarynat" onclick="rating(2);" id="paaryna2"><img  src="paaryna.png" width="30px" height="30px" alt="Image not found" id="p2"></button>
-                        <button type="button" class="btn paarynat" onclick="rating(3);" id="paaryna3"><img  src="paaryna.png" width="30px" height="30px" alt="Image not found" id="p3"></button>
-                        <button type="button" class="btn paarynat" onclick="rating(4);" id="paaryna4"><img  src="paaryna.png" width="30px" height="30px" alt="Image not found" id="p4"></button>
-                        <button type="button" class="btn paarynat" onclick="rating(5);" id="paaryna5"><img  src="paaryna.png" width="30px" height="30px" alt="Image not found" id="p5"></button>
-                        <input type="hidden"  name="arvio" id="arvio" ></div>
-                        <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mb-4"><input class="form-control form-control-lg" type="submit" name="submit"></div>
+                        <div class="col-md-6 col-lg-3 col-xl-3 mx-auto mb-4"><input class="form-control form-control-lg" id="nimikentta" type="text" placeholder="Kirjoita tähän nimesi.." name="name"></div>
+                        <div class="col-md-6 col-lg-3 col-xl-3 mx-auto mb-4"><input class="form-control form-control-lg"  id="tekstikentta" type="text" placeholder="Kirjoita tähän viesti..." name="message"></div>
+                        <div class="col-md-6 col-lg-4 col-xl-4 mx-auto mb-4 rating">
+                          <button type="button" class="btn paarynat " onclick="rating(1);" id="paaryna1"><img  src="../media/paaryna.png" width="30px" height="30px" alt="Image not found" id="p1"></button>
+                          <button type="button" class="btn paarynat" onclick="rating(2);" id="paaryna2"><img  src="../media/paaryna.png" width="30px" height="30px" alt="Image not found" id="p2"></button>
+                          <button type="button" class="btn paarynat" onclick="rating(3);" id="paaryna3"><img  src="../media/paaryna.png" width="30px" height="30px" alt="Image not found" id="p3"></button>
+                          <button type="button" class="btn paarynat" onclick="rating(4);" id="paaryna4"><img  src="../media/paaryna.png" width="30px" height="30px" alt="Image not found" id="p4"></button>
+                          <button type="button" class="btn paarynat" onclick="rating(5);" id="paaryna5"><img  src="../media/paaryna.png" width="30px" height="30px" alt="Image not found" id="p5"></button>
+                          <input type="hidden"  name="arvio" id="arvio">
+                        </div>
+                        <div class="col-md-6 col-lg-2 col-xl-2 mx-auto mb-4"><input class="form-control form-control-lg" type="submit" name="submit"></div>
                     </div>
                   </form>
+           
+                  
+            </div>
+            <div class="row" style="height: 5svh;"></div> <!-- spacer -->
         </div>
-
-        <!-- Maps -->
-        <!--<div class="container" style="text-align: center;">
-        <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3372.381422151354!2d25.51096114198672!3d64.9996731497764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4681cd4546b29057%3A0x2a9ad59238666eb7!2sOSAO%2C%20Kaukovainio%2C%20Technical%20unit!5e0!3m2!1sen!2sfi!4v1696400885824!5m2!1sen!2sfi"
-            width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"></iframe>
-        </div>-->
-        
-
-
+ 
 <!-- Footer -->
 <footer class="text-center text-lg-start bg-light text-muted">
 
